6d8978422447de72e0dff89068993963653aed65
Leonard Constantin <leo.marinel13@yahoo.com>
2023-11-15 23:06:08 +0200

    created time-slots service and initialized small crud on the timeSlots subcollection

---
+++ src/app/core/services/time-slots.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TimeSlotsService {
+  constructor(private firestore: AngularFirestore) {}
+
+  addTimeSlots(doctorId: string, date: string | undefined, availableSlots: string[]) {
+    const doctorRef = this.firestore.collection('doctors').doc(doctorId);
+    const timeSlotsRef = doctorRef.collection('timeSlots').doc(date);
+
+    return timeSlotsRef.set({ availableSlots });
+  }
+
+  getTimeSlots(doctorId: string | undefined, date: string | undefined) {
+    const doctorRef = this.firestore.collection('doctors').doc(doctorId);
+    const timeSlotsRef = doctorRef.collection('timeSlots').doc(date);
+
+    return timeSlotsRef.get();
+  }
+
+  updateTimeSlots(doctorId: string, date: string | undefined, availableSlots: string[]) {
+    const doctorRef = this.firestore.collection('doctors').doc(doctorId);
+    const timeSlotsRef = doctorRef.collection('timeSlots').doc(date);
+
+    return timeSlotsRef.update({ availableSlots });
+  }
+
+  deleteTimeSlots(doctorId: string, date: string | undefined) {
+    const doctorRef = this.firestore.collection('doctors').doc(doctorId);
+    const timeSlotsRef = doctorRef.collection('timeSlots').doc(date);
+
+    return timeSlotsRef.delete();
+  }
+}
