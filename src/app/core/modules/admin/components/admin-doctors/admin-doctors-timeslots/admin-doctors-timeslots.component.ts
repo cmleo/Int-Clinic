4a7d6a17f1e543b017b3aae4950bad866746e40b
cmleo <leo.marinel13@yahoo.com>
2023-11-26 16:21:01 +0200

    now the admin is able to add and remove time slots for any doctor in any day

--- src/app/core/modules/admin/components/admin-doctors/admin-doctors-timeslots/admin-doctors-timeslots.component.ts
+++ src/app/core/modules/admin/components/admin-doctors/admin-doctors-timeslots/admin-doctors-timeslots.component.ts
@@ -13,21 +13,16 @@ export class AdminDoctorsTimeslotsComponent implements OnInit {
     private timeSlotsService: TimeSlotsService
   ) {}
 
-  ngOnInit(): void {
-    this.doctorId = this.route.snapshot.params['id'];
-
-    this.timeSlotsService.getTimeSlots(this.doctorId, this.currentLocalDate).subscribe(data => {
-      this.doctorTimeSlots = data.data()?.['availableSlots'];
-      console.log(this.doctorTimeSlots);
-    });
-  }
-
   currentLocalDate = this.formatDate(new Date());
 
-  slotSelected!: string;
-  doctorId!: string;
   currentDate = new Date();
+  doctorId!: string;
+  doctorSlotsSelected: string[] = [];
+  addableSlotsSelected: string[] = [];
   doctorTimeSlots: string[] = [];
+  defaultTimeSlots: string[] = ['09:00', '10:00', '11:00', '12:00', '14:00', '15:00', '16:00', '17:00'];
+  addableTimeSlots: string[] = [];
+  updatedTimeSlots: string[] = [];
 
   selected: Date | null = new Date();
   dateSelected: Date | null = null;
@@ -38,20 +33,30 @@ export class AdminDoctorsTimeslotsComponent implements OnInit {
     return day !== 0 && day !== 6;
   };
 
-  isActive(slot: string) {
-    return this.slotSelected === slot;
-  }
-
-  onSelectTimeSlot(slot: string) {
-    this.slotSelected = slot;
-  }
-
   private formatDate(date: Date): string {
     return date.toLocaleDateString('ro-Ro', {
       day: 'numeric',
       month: 'short',
       year: 'numeric',
     });
+  }
+
+  ngOnInit(): void {
+    this.doctorId = this.route.snapshot.params['id'];
+
+    this.timeSlotsService.getTimeSlots(this.doctorId, this.currentLocalDate).subscribe(data => {
+      this.doctorTimeSlots = data.data()?.['availableSlots'].sort() || [];
+
+      if (!this.doctorTimeSlots) {
+        return;
+      }
+
+      this.addableTimeSlots = this.defaultTimeSlots.filter(slot => !this.doctorTimeSlots.includes(slot)).sort();
+    });
+
+    this.addableTimeSlots = [];
+    this.doctorSlotsSelected = [];
+    this.addableSlotsSelected = [];
   }
 
   onClickDate() {
@@ -61,8 +66,88 @@ export class AdminDoctorsTimeslotsComponent implements OnInit {
       this.localDateSelected = this.formatDate(this.dateSelected);
 
       this.timeSlotsService.getTimeSlots(this.doctorId, this.localDateSelected).subscribe(timeSlots => {
-        this.doctorTimeSlots = timeSlots.data()?.['availableSlots'];
+        this.doctorTimeSlots = timeSlots.data()?.['availableSlots'].sort() || [];
+
+        if (!this.doctorTimeSlots) {
+          return;
+        }
+
+        this.addableTimeSlots = this.defaultTimeSlots.filter(slot => !this.doctorTimeSlots.includes(slot)).sort();
       });
+      this.addableTimeSlots = [];
+      this.doctorSlotsSelected = [];
+      this.addableSlotsSelected = [];
+    }
+  }
+
+  onSelectAddableTimeSlot(slot: string) {
+    const index = this.addableSlotsSelected.indexOf(slot);
+    if (index !== -1) {
+      this.addableSlotsSelected.splice(index, 1);
+    } else {
+      this.addableSlotsSelected.push(slot);
+    }
+  }
+
+  onSelectDoctorTimeSlot(slot: string) {
+    const index = this.doctorSlotsSelected.indexOf(slot);
+    if (index !== -1) {
+      this.doctorSlotsSelected.splice(index, 1);
+    } else {
+      this.doctorSlotsSelected.push(slot);
+    }
+  }
+
+  isActive(slot: string) {
+    return this.doctorSlotsSelected.includes(slot);
+  }
+
+  isSelected(slot: string) {
+    return this.addableSlotsSelected.includes(slot);
+  }
+
+  onAddTimeSlots() {
+    if (this.addableSlotsSelected.length > 0) {
+      this.updatedTimeSlots = [...this.doctorTimeSlots, ...this.addableSlotsSelected].sort();
+
+      if (this.doctorTimeSlots.length > 0) {
+        this.timeSlotsService.updateTimeSlots(
+          this.doctorId,
+          this.localDateSelected || this.currentLocalDate,
+          this.updatedTimeSlots
+        );
+      } else {
+        this.timeSlotsService.postTimeSlots(
+          this.doctorId,
+          this.localDateSelected || this.currentLocalDate,
+          this.addableSlotsSelected.sort()
+        );
+      }
+
+      this.doctorTimeSlots = this.updatedTimeSlots.sort();
+      this.addableTimeSlots = this.defaultTimeSlots.filter(slot => !this.updatedTimeSlots.includes(slot)).sort();
+
+      this.addableSlotsSelected = [];
+      this.doctorSlotsSelected = [];
+    }
+  }
+
+  onDeleteTimeSlots() {
+    if (this.doctorSlotsSelected.length > 0) {
+      this.updatedTimeSlots = this.doctorTimeSlots.filter(slot => !this.doctorSlotsSelected.includes(slot));
+
+      this.timeSlotsService.updateTimeSlots(
+        this.doctorId,
+        this.localDateSelected || this.currentLocalDate,
+        this.updatedTimeSlots
+      );
+
+      this.doctorTimeSlots = this.updatedTimeSlots.sort();
+      this.addableTimeSlots = this.defaultTimeSlots.filter(slot => !this.updatedTimeSlots.includes(slot)).sort();
+    }
+
+    if (this.doctorTimeSlots.length === 0) {
+      this.timeSlotsService.deleteTimeSlots(this.doctorId, this.localDateSelected || this.currentLocalDate);
     }
   }
 }
